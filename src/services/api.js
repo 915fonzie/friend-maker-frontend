8e7108b1563eaded4e413cc6092cdfd10a7ff1ec
github username <github email address>
2020-01-10 09:27:13 -0800

    added gitignore file, created Login component

---
+++ src/services/api.js
@@ -0,0 +1,62 @@
+const API_ROOT = `http://localhost:3000/api/v1`;
+const token = localStorage.getItem('token');
+
+const headers = {
+    'Content-Type': 'application/json',
+    Accepts: 'application/json',
+    Authorization: token
+};
+
+const login = data => {
+    return fetch(`${API_ROOT}/auth`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+    })
+        .then(resp => resp.json());
+};
+
+const signup = data => {
+    return fetch(`${API_ROOT}/users`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+    })
+        .then(resp => resp.json());
+}
+
+const getCurrentUser = () => {
+    return fetch(`${API_ROOT}/current_user`, {
+        headers
+    })
+        .then(resp => {
+            return resp.json();
+        });
+};
+
+const getMatchingUsersFromGreatest = interests => {
+    return fetch(`${API_ROOT}/find_matches_by_greatest?filtered_interests=${interests}`, {
+        headers: headers
+    })
+    .then(resp => resp.json())
+}
+
+const getMatchingUsersFromLeast = interests => {
+  return fetch(
+    `${API_ROOT}/find_matches_by_least?filtered_interests=${interests}`,
+    {
+      headers: headers
+    }
+  ).then(resp => resp.json());
+};
+
+export const api = {
+    auth: {
+        login,
+        getCurrentUser
+    },
+    matchedUsers: {
+        getMatchingUsersFromGreatest,
+        getMatchingUsersFromLeast
+    }
+}
